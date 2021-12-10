80545c398d2e1c2589c62477597d90a9dee51eef
Frd0mF <ibrahimbouslime@gmail.com>
2021-12-10 14:40:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import * as ROUTES from './constants/routes'
 import AuthListener from "./hooks/AuthListener"
 import UserContext from './context/user'
+import ProtectedRoute from "./helpers/protected.route"
+import isLoggedIn from "./helpers/isLoggedIn"
 
 const Login = lazy(() => import ('./pages/Login'))
 const Signup = lazy(() => import ('./pages/Signup'))
@@ -22,8 +24,14 @@ export default function App() {
      {/*use suspense to load this component while user is waitinf for requested page to load*/}
      <Suspense fallback={<h1>LOADING</h1>}>
       <Routes>
-        <Route path={ROUTES.LOGIN} element={<Login/>} />
-        <Route path={ROUTES.HOMEPAGE} element={<Homepage/>} />
+      <Route path={ROUTES.LOGIN} element={
+            <isLoggedIn user={user}>
+              <Login/>
+              </isLoggedIn>} />
+      <Route path={ROUTES.HOMEPAGE} element={
+            <ProtectedRoute user={user}>
+              <Homepage/>
+            </ProtectedRoute>} />
         <Route path={ROUTES.SIGN_UP} element={<Signup/>} />
         <Route path={ROUTES.PROFILE} element={<Profile/>} />
         <Route path='*' element={<NotFound/>} />
